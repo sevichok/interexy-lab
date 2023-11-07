54a0ab11128f6bae627e4ad5edf61e863e7b6c68
sevichok <90391121+sevichok@users.noreply.github.com>
2023-11-07 11:04:34 +0300

    lesson 2 init

--- README.md
+++
@@ -1,3 +0,0 @@
-Interexy-lab repository
-
-1. Lesson 1 - Sorting
--- lesson-2/app.js
+++ lesson-2/app.js
@@ -1,17 +1,4 @@
-let array = Array.from({ length: 100000 }, () => { Math.floor(Math.random() * 100000) })
-
-// const quickSort = (arr) => {
-//     if (arr.length < 2) {
-//         return arr
-//     } else {
-//         let pivotIndex = Math.floor(arr.length / 2)
-//         let pivot = arr[pivotIndex]
-//         arr.splice(pivotIndex, 1);
-//         let leftArr = arr.filter(el => el <= pivot)
-//         let rightArr = arr.filter(el => el > pivot)
-//         return quickSort(leftArr).concat(pivot, quickSort(rightArr))
-//     }
-// }
+const array = Array.from({ length: 100000 }, () => { Math.floor(Math.random() * 100000) })
 
 const quickSort = (arr) => {
     if (arr.length < 2) {
