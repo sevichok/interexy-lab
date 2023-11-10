ff7aa07294b34af6764f4baccf7d240d3bd2fe2b
sevichok <90391121+sevichok@users.noreply.github.com>
2023-11-10 15:13:41 +0300

    made cube moving 4 different ways in lesson-3

--- lesson-3/index.js
+++ lesson-3/index.js
@@ -1,1 +1,44 @@
-console.log('run index.js')
+const cube3 = document.getElementById('cube3')
+const row3 = document.getElementById('row3')
+const cube4 = document.getElementById('cube4')
+const row4 = document.getElementById('row4')
+
+const move3 = (elem) => {
+    setTimeout(() => {
+        elem.classList.add('up')
+    }, 0);
+    setTimeout(() => {
+        elem.classList.add('down')
+        elem.classList.remove('up')
+    }, 4000);
+    elem.classList.remove('down')
+    if (cube3) {
+        setTimeout(() => {
+            move3(cube3)
+        }, 8000)
+    }
+}
+move3(cube3)
+
+let position = 0;
+let direction = 'right';
+let width = row4.offsetWidth
+
+function move4() {
+    if (direction == 'right') {
+        position += 2
+        cube4.style.transform = `translateX(${position}px)`
+        if (position > width - 20) {
+            direction = 'left';
+        }
+    }
+    if (direction == 'left') {
+        position -= 2
+        cube4.style.transform = `translateX(${position}px)`
+        if (position == 0) {
+            direction = 'right';
+        }
+    }
+    requestAnimationFrame(move4)
+}
+window.requestAnimationFrame(move4);
