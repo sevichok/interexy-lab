13bf44f41660274fd7a7909e9fd1be53ddc3e71d
sevichok <90391121+sevichok@users.noreply.github.com>
2023-11-13 17:14:27 +0300

    lesson3 with promises

--- lesson-3/index.js
+++ lesson-3/index.js
@@ -42,3 +42,43 @@ function move4() {
     requestAnimationFrame(move4)
 }
 window.requestAnimationFrame(move4);
+
+let array = Array.from({ length: 50000 }, () => { Math.floor(Math.random() * 50000) })
+
+// const bubbleSort = (arr) => {
+//     return new Promise((resolve) => {
+//         const length = arr.length
+//         for (let i = 0; i < length; i++) {
+//             for (let j = 0; j < length; j++) {
+//                 if (arr[j + 1] < arr[j]) {
+//                     const swapElem = arr[j]
+//                     arr[j] = arr[j + 1]
+//                     arr[j + 1] = swapElem
+//                 }
+//             }
+//         }
+//         return resolve(arr)
+//     })
+// }
+const promise = async () => await new Promise((resolve) => resolve(1))
+const promise2 = async (ms) => await new Promise((resolve) => setTimeout(resolve(1), ms))
+
+const bubbleSort = (arr) => {
+    const length = arr.length
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
+            if (arr[j + 1] < arr[j]) {
+                promise2(0).then()
+                const swapElem = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = swapElem
+            }
+        }
+    }
+    return arr
+}
+
+const start = new Date().getTime();
+bubbleSort(array)
+const end = new Date().getTime();
+console.log(`BubbleSort: ${end - start}ms`);
