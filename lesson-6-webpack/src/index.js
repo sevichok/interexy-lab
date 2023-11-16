4b35dc0ca54460c18f8d6ad1fee112d7e483d519
sevichok <90391121+sevichok@users.noreply.github.com>
2023-11-16 11:37:11 +0300

    gitignore

---
+++ lesson-6-webpack/src/index.js
@@ -0,0 +1,53 @@
+import getCharacters from "./api"
+
+const cube3 = document.getElementById('cube3')
+const row3 = document.getElementById('row3')
+const cube4 = document.getElementById('cube4')
+const row4 = document.getElementById('row4')
+
+const characterUrl = 'https://rickandmortyapi.com/api/character'
+getCharacters(characterUrl)
+
+const worker = new Worker('../src/worker.js')
+
+const move3 = (elem) => {
+    setTimeout(() => {
+        elem.classList.add('up')
+    }, 0);
+    setTimeout(() => {
+        elem.classList.add('down')
+        elem.classList.remove('up')
+    }, 4000);
+    elem.classList.remove('down')
+    if (cube3) {
+        setTimeout(() => {
+            move3(cube3)
+        }, 8000)
+    }
+}
+move3(cube3)
+
+let position = 0;
+let direction = 'right';
+let width = row4.offsetWidth
+
+function move4() {
+    if (direction == 'right') {
+        position += 2
+        cube4.style.transform = `translateX(${position}px)`
+        if (position > width - 20) {
+            direction = 'left';
+        }
+    }
+    if (direction == 'left') {
+        position -= 2
+        cube4.style.transform = `translateX(${position}px)`
+        if (position == 0) {
+            direction = 'right';
+        }
+    }
+    requestAnimationFrame(move4)
+}
+window.requestAnimationFrame(move4);
+
+console.log('ts with webpack')
