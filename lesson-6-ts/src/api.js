09d51bb1dabadd03d52a868a098df34e51df7d7f
sevichok <90391121+sevichok@users.noreply.github.com>
2023-11-16 11:33:27 +0300

    everything in webpack without ts

---
+++ lesson-6-ts/src/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+// const characterUrl = 'https://rickandmortyapi.com/api/character'
+
+const getCharacters = (url) => {
+    axios.get(url)
+        .then(response => {
+            const length = response.data.info.pages
+            const array = []
+            for (let elem = 0; elem < length; elem++) {
+                if (elem % 2 == 0) {
+                    array.push(axios.get(url + `/?page=${elem}`))
+                }
+            }
+            return Promise.all(array)
+        }
+        ).then(response => {
+            const results = response.map(elem => elem.data.results)
+            console.log(results.length)
+            return results
+        })
+
+}
+export default getCharacters
